dcfd81fb059c7d193b47b3bc29515ec368451241
Whil Piavis <ncsupiratecaptain@gmail.com>
2018-01-23 20:59:09 -0700

    Completes search with caching feature for jobs index.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,14 +5,17 @@ class JobsController < ApplicationController
   end
 
   def index
-    @jobs = Rails.cache.fetch(@cache_key) do 
-      rebuild_cache(@cache_key, Job)
-    end
+    @jobs = []
     # handle a search.
-    if search_params['title'] || search_params['description']
-      @jobs = search_job_titles(search_params['title'])
-      #@jobs.select!{ |j| j.title == search_params['title'] } if search_params['title']
-      #@jobs.select!{ |j| j.description == search_params['description'] } if search_params['description']
+    search_p = search_params # Initialize search_p so we don't have to keep calling search_params.
+    if search_p['title'] || search_p['description']
+      @jobs = []
+      @jobs += search_job(search_params['title'], 'title') if search_p['title']
+      @jobs += search_job(search_params['description'], 'description') if search_p['description']
+    else
+      @jobs = Rails.cache.fetch(@cache_key) do 
+        rebuild_cache(@cache_key, Job)
+      end
     end
   end
 
@@ -22,45 +25,45 @@ class JobsController < ApplicationController
 
   private
 
-  # Returns the parameters for the search.
-  def search_params
-    params.delete(:page) # Remove :page so we don't see unpermitted parameter warnings.
-    params.permit(:title, :description)
-  end
-
-  # A fuzzier search based on job title words.
-  # Returns an array of jobs with titles who's terms match the search_words.
-  # This could be improved by looking for the 'distance between' the search terms
-  # and the Job title's terms, but that would require some more time consuming/in depth algorithms.
-  def search_job_titles(search_words)
-    jobs = [] # Array of jobs matching search words to return.
-    # As building the title_keywords is fairly expensive and slow, cache it.
-    title_keywords = Rails.cache.fetch(:title_keywords) do 
-      build_title_keywords
+    # Returns the parameters for the search.
+    def search_params
+      params.delete(:page) # Remove :page so we don't see unpermitted parameter warnings.
+      params.permit(:title, :description)
     end
-    search_words.split(' ').each do |w|
-      if title_keywords.has_key?(w.downcase.to_sym)
-        title_keywords[w.downcase.to_sym].each do |job_id|
-          jobs << Job.find(job_id)
+
+    # A fuzzier search based on job title and description words.
+    # Returns an array of jobs with attributes who's terms match the search_words.
+    # This could be improved by looking for the 'distance between' the search terms
+    # and the Job attributes's terms, but that would require some more time consuming/in depth algorithms.
+    def search_job(search_words, attribute)
+      jobs = [] # Array of jobs matching search words to return.
+      # As building the attribute_keywords is fairly expensive and slow, cache it.
+      keywords = Rails.cache.fetch("#{attribute}_keywords".to_sym) do 
+        build_keywords(attribute)
+      end
+      search_words.split(' ').each do |w|
+        if keywords.has_key?(w.downcase.to_sym)
+          keywords[w.downcase.to_sym].each do |job_id|
+            jobs << Job.find(job_id)
+          end
         end
       end
+      jobs
     end
-    jobs
-  end
 
-  # Builds a hash with keys of Job title terms, and values of job ids (memoization!).
-  def build_title_keywords
-    title_keywords = {}
-    Job.all.each do |job|
-      job.title.split(' ').each do |word|
-        if title_keywords.has_key?(word.downcase.to_sym)
-          title_keywords[word.downcase.to_sym] << job.id
-        else
-          title_keywords[word.downcase.to_sym] = [job.id]
+    # Builds a hash with keys of Job title or description terms, and values of job ids (memoization!).
+    def build_keywords(attribute)
+      keywords = {}
+      Job.all.each do |job|
+        job.send(attribute).split(' ').each do |word|
+          if keywords.has_key?(word.downcase.to_sym)
+            keywords[word.downcase.to_sym] << job.id
+          else
+            keywords[word.downcase.to_sym] = [job.id]
+          end
         end
       end
+      keywords
     end
-    title_keywords
-  end
 
 end
